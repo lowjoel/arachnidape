4e07e9684008b66134ce07b93106a8c854db62c5
Joel Low <joel@joelsplace.sg>
2012-09-22 10:19:07 +0000

    Run our shell spawning code in a separate function. We need to do other stuff in main.

--- JSShell/JSShell.cpp
+++ JSShell/JSShell.cpp
@@ -13,101 +13,107 @@ namespace {
 
 	unsigned int __stdcall CopyOutput(void* arg);
 	void CopyOutput(const CopyOutputArguments& arg);
+
+	void StartJavaScriptShell();
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	STARTUPINFO jsShellStartupInfo = { 0 };
-	PROCESS_INFORMATION jsShellInfo = { 0 };
-
-	//Create our command line.
-	TCHAR cmdLine[MAX_PATH];
-	_tcscpy_s(cmdLine, _T("js -f Stub.js -i"));
-
-	//Create our process creation information.
-	jsShellStartupInfo.cb = sizeof(jsShellStartupInfo);
-	jsShellStartupInfo.dwFlags = STARTF_USESTDHANDLES;
-
-	//Create our redirected pipes
-	KernelHandle stdInRead, stdInWrite;
-	KernelHandle stdOutRead, stdOutWrite;
-	KernelHandle stdErrRead, stdErrWrite;
-	{
-		HANDLE hStdInRead, hStdInWrite;
-		HANDLE hStdOutRead, hStdOutWrite;
-		HANDLE hStdErrRead, hStdErrWrite;
-
-		//We need an inheritable security permission
-		SECURITY_ATTRIBUTES saAttr; 
-		saAttr.nLength = sizeof(SECURITY_ATTRIBUTES); 
-		saAttr.bInheritHandle = TRUE; 
-		saAttr.lpSecurityDescriptor = NULL; 
-
-		if (!CreatePipe(&hStdInRead, &hStdInWrite, &saAttr, 0))
-			throw GetLastError();
-		stdInRead = KernelHandle(hStdInRead);
-		stdInWrite = KernelHandle(hStdInWrite);
-
-		//Don't let the child process inherit the stdin write handle.
-		SetHandleInformation(stdInWrite.get(), HANDLE_FLAG_INHERIT, 0);
-
-		if (!CreatePipe(&hStdOutRead, &hStdOutWrite, &saAttr, 0))
-			throw GetLastError();
-		stdOutRead = KernelHandle(hStdOutRead);
-		stdOutWrite = KernelHandle(hStdOutWrite);
-
-		//Don't let the child process inherit the stdin write handle.
-		SetHandleInformation(stdOutRead.get(), HANDLE_FLAG_INHERIT, 0);
-
-		if (!CreatePipe(&hStdErrRead, &hStdErrWrite, &saAttr, 0))
-			throw GetLastError();
-		stdErrRead = KernelHandle(hStdErrRead);
-		stdErrWrite = KernelHandle(hStdErrWrite);
-
-		//Don't let the child process inherit the stdin write handle.
-		SetHandleInformation(stdErrRead.get(), HANDLE_FLAG_INHERIT, 0);
-	}
-	
-	jsShellStartupInfo.hStdInput = stdInRead.get();
-	jsShellStartupInfo.hStdOutput = stdOutWrite.get();
-	jsShellStartupInfo.hStdError = stdErrWrite.get();
-	
-	//Create the process.
-	KernelHandle jsShellProcess;
-	KernelHandle jsShellThread;
-	if (!CreateProcess(nullptr, cmdLine, nullptr, nullptr, true, 0,
-		nullptr, nullptr, &jsShellStartupInfo, &jsShellInfo))
-	{
-		unsigned error = GetLastError();
-		_tprintf_s(_T("Could not start Mozilla JavaScript shell. Is js.exe in ")
-			_T("your path? [GetLastError()=%d]"), error);
-		return error;
-	}
-
-	//Assign the handle to auto-destruct
-	jsShellProcess = KernelHandle(jsShellInfo.hProcess);
-	jsShellThread = KernelHandle(jsShellInfo.hThread);
-
-	//Create threads to handle stdout and stderr
-	KernelHandle thisStdOutWrite = GetStdHandle(STD_OUTPUT_HANDLE);
-	CopyOutputArguments stdOutReaderArgs = { stdOutRead, thisStdOutWrite };
-	_beginthreadex(nullptr, 0, &CopyOutput, &stdOutReaderArgs, 0, nullptr);
-
-	KernelHandle thisStdErrWrite = GetStdHandle(STD_ERROR_HANDLE);
-	CopyOutputArguments stdErrReaderArgs = { stdErrRead, thisStdErrWrite };
-	_beginthreadex(nullptr, 0, &CopyOutput, &stdErrReaderArgs, 0, nullptr);
-
-	KernelHandle thisStdInRead = GetStdHandle(STD_INPUT_HANDLE);
-	CopyOutputArguments stdInReaderArgs = { thisStdInRead, stdInWrite };
-	_beginthreadex(nullptr, 0, &CopyOutput, &stdInReaderArgs, 0, nullptr);
-
-	//Wait for the process to terminate
-	WaitForSingleObject(jsShellProcess.get(), INFINITE);
-
-	return 0;
+	StartJavaScriptShell();
 }
 
 namespace {
+
+	void StartJavaScriptShell()
+	{
+		STARTUPINFO jsShellStartupInfo = { 0 };
+		PROCESS_INFORMATION jsShellInfo = { 0 };
+
+		//Create our command line.
+		TCHAR cmdLine[MAX_PATH];
+		_tcscpy_s(cmdLine, _T("js -f Stub.js -i"));
+
+		//Create our process creation information.
+		jsShellStartupInfo.cb = sizeof(jsShellStartupInfo);
+		jsShellStartupInfo.dwFlags = STARTF_USESTDHANDLES;
+
+		//Create our redirected pipes
+		KernelHandle stdInRead, stdInWrite;
+		KernelHandle stdOutRead, stdOutWrite;
+		KernelHandle stdErrRead, stdErrWrite;
+		{
+			HANDLE hStdInRead, hStdInWrite;
+			HANDLE hStdOutRead, hStdOutWrite;
+			HANDLE hStdErrRead, hStdErrWrite;
+
+			//We need an inheritable security permission
+			SECURITY_ATTRIBUTES saAttr; 
+			saAttr.nLength = sizeof(SECURITY_ATTRIBUTES); 
+			saAttr.bInheritHandle = TRUE; 
+			saAttr.lpSecurityDescriptor = NULL; 
+
+			if (!CreatePipe(&hStdInRead, &hStdInWrite, &saAttr, 0))
+				throw GetLastError();
+			stdInRead = KernelHandle(hStdInRead);
+			stdInWrite = KernelHandle(hStdInWrite);
+
+			//Don't let the child process inherit the stdin write handle.
+			SetHandleInformation(stdInWrite.get(), HANDLE_FLAG_INHERIT, 0);
+
+			if (!CreatePipe(&hStdOutRead, &hStdOutWrite, &saAttr, 0))
+				throw GetLastError();
+			stdOutRead = KernelHandle(hStdOutRead);
+			stdOutWrite = KernelHandle(hStdOutWrite);
+
+			//Don't let the child process inherit the stdin write handle.
+			SetHandleInformation(stdOutRead.get(), HANDLE_FLAG_INHERIT, 0);
+
+			if (!CreatePipe(&hStdErrRead, &hStdErrWrite, &saAttr, 0))
+				throw GetLastError();
+			stdErrRead = KernelHandle(hStdErrRead);
+			stdErrWrite = KernelHandle(hStdErrWrite);
+
+			//Don't let the child process inherit the stdin write handle.
+			SetHandleInformation(stdErrRead.get(), HANDLE_FLAG_INHERIT, 0);
+		}
+	
+		jsShellStartupInfo.hStdInput = stdInRead.get();
+		jsShellStartupInfo.hStdOutput = stdOutWrite.get();
+		jsShellStartupInfo.hStdError = stdErrWrite.get();
+	
+		//Create the process.
+		KernelHandle jsShellProcess;
+		KernelHandle jsShellThread;
+		if (!CreateProcess(nullptr, cmdLine, nullptr, nullptr, true, 0,
+			nullptr, nullptr, &jsShellStartupInfo, &jsShellInfo))
+		{
+			unsigned error = GetLastError();
+			_tprintf_s(_T("Could not start Mozilla JavaScript shell. Is js.exe in ")
+				_T("your path? [GetLastError()=%d]"), error);
+			throw error;
+		}
+
+		//Assign the handle to auto-destruct
+		jsShellProcess = KernelHandle(jsShellInfo.hProcess);
+		jsShellThread = KernelHandle(jsShellInfo.hThread);
+
+		//Create threads to handle stdout and stderr
+		KernelHandle thisStdOutWrite = GetStdHandle(STD_OUTPUT_HANDLE);
+		CopyOutputArguments stdOutReaderArgs = { stdOutRead, thisStdOutWrite };
+		_beginthreadex(nullptr, 0, &CopyOutput, &stdOutReaderArgs, 0, nullptr);
+
+		KernelHandle thisStdErrWrite = GetStdHandle(STD_ERROR_HANDLE);
+		CopyOutputArguments stdErrReaderArgs = { stdErrRead, thisStdErrWrite };
+		_beginthreadex(nullptr, 0, &CopyOutput, &stdErrReaderArgs, 0, nullptr);
+
+		KernelHandle thisStdInRead = GetStdHandle(STD_INPUT_HANDLE);
+		CopyOutputArguments stdInReaderArgs = { thisStdInRead, stdInWrite };
+		_beginthreadex(nullptr, 0, &CopyOutput, &stdInReaderArgs, 0, nullptr);
+
+		//Wait for the process to terminate
+		WaitForSingleObject(jsShellProcess.get(), INFINITE);
+	}
+
 	unsigned int __stdcall CopyOutput(void* arg)
 	{
 		CopyOutputArguments& argument =
